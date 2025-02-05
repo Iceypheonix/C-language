09716a40e1e5f0301e9c6bc978ae38ebfd96535e
RahulCh0udhary108 <rahulchoudharyy90@gmail.com>
2025-02-05 16:48:02 +0530

    Update NumberGame.c

--- NumberGame.c
+++ NumberGame.c
@@ -2,27 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+int main()
+{
     int number, guess, attempts = 0;
 
-    // Seed the random number generator
     srand(time(0));
-    number = rand() % 100 + 1; // Generate a random number between 1 and 100
+    number = rand() % 100 + 1;
 
     printf("Welcome to the Number Guessing Game!\n");
     printf("I have selected a number between 1 and 100. Try to guess it.\n");
 
-    // Loop until the user guesses the correct number
-    do {
+    do
+    {
         printf("Enter your guess: ");
         scanf("%d", &guess);
         attempts++;
 
-        if (guess > number) {
+        if (guess > number)
+        {
             printf("Too high! Try again.\n");
-        } else if (guess < number) {
+        }
+        else if (guess < number)
+        {
             printf("Too low! Try again.\n");
-        } else {
+        }
+        else
+        {
             printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
         }
     } while (guess != number);
